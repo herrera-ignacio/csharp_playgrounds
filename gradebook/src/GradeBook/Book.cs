77e6a2f2e097403022442e5a52f4b3c44ad24165
Ignacio Herrera <ignacioromanherrera@gmail.com>
2020-04-21 22:47:12 -0300

    Gradebook - Flow of execution

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -16,7 +16,14 @@ namespace GradeBook
 
         public void AddGrade(double grade)
         {
-            this.grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
         }
 
         public Statistics GetStatistics()
@@ -33,6 +40,29 @@ namespace GradeBook
                result.Average += number;
             }
             result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,14 +8,43 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Test Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            
+            while(true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+                
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+
             var stats = book.GetStatistics();
           
             Console.WriteLine($"Lowest grade is {stats.Low}");
             Console.WriteLine($"Highest grade is {stats.High}");
             Console.WriteLine($"Average grade is {stats.Average}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
             
             if (args.Length > 0)
             {
